30de6ba234b251b39c23df5512253a291645e540
svanzee4cb <svanzee@Van-Zees-MacBook-Pro.local>
2014-12-09 13:40:41 -0600

    first commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'role-rabbitmq'
+maintainer       'Stephen Van Zee'
+maintainer_email 'svanzee@caringbridge.org'
+license          'All rights reserved'
+description      'Installs/Configures role-rabbitmq'
+long_description 'Installs/Configures role-rabbitmq'
+version          '0.1.0'
+
+depends 'rabbitmq'
---
+++ recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: role-rabbitmq
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'rabbitmq'
